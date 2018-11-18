36d225bfbc05a9693ce8f7f3a55b1a0e1c72f373
Freddie Carthy <fcarthy@gmail.com>
2018-11-17 23:18:16 -0800

    fzf for git, wip

--- conf/bash/helpers/aliases.bash
+++ conf/bash/helpers/aliases.bash
@@ -1,3 +1,4 @@
 alias ls='ls -la'
 alias play='cd ~/Projects/'
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+alias log="git log --pretty=oneline --abbrev-commit | fzf --preview 'echo {} | cut -f 1 -d \" \" | xargs git show --color=always'"
--- conf/bash/helpers/fzf.bash
+++ conf/bash/helpers/fzf.bash
@@ -1,1 +1,5 @@
 export FZF_DEFAULT_OPTS="--preview 'cat {}'"
+# export FZF_DEFAULT_OPTS='
+#   --color fg:124,bg:16,hl:202,fg+:214,bg+:52,hl+:231
+#   --color info:52,prompt:196,spinner:208,pointer:196,marker:208
+# '
