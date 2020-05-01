a458c8af573af1f041d565c6bef3b5271988ab9d
Freddie Carthy <fcarthy@gmail.com>
2020-04-30 23:50:34 -0700

    update symlinks

--- conf/runcom/bash_profile.sh
+++ conf/runcom/bash_profile.sh
@@ -8,14 +8,3 @@ source /usr/local/opt/dotfiles/conf/bash/prompt.sh
 if [ -f ~/.bash_profile.local ]; then
   source ~/.bash_profile.local
 fi
-
-source ~/.bash/aliases.sh
-source ~/.bash/colors.sh
-source ~/.bash/fzf.sh
-source ~/.bash/git_completion.sh
-source ~/.bash/paths.sh
-source ~/.bash/prompt.sh
-
-if [ -f ~/.bash_profile.local ]; then
-  source ~/.bash_profile.local
-fi
