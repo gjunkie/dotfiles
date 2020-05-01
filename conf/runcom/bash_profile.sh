0dce26a9fbb3b755f2e3aab9d0d82142636be836
Freddie Carthy <fcarthy@gmail.com>
2020-05-01 00:12:08 -0700

    source typo

--- conf/runcom/bash_profile.sh
+++ conf/runcom/bash_profile.sh
@@ -1,7 +1,7 @@
 source /usr/local/opt/dotfiles/conf/bash/aliases.sh
 source /usr/local/opt/dotfiles/conf/bash/colors.sh
 source /usr/local/opt/dotfiles/conf/bash/fzf.sh
-source /usr/local/opt/dotfiles/conf/bash/git_completions.sh
+source /usr/local/opt/dotfiles/conf/bash/git_completion.sh
 source /usr/local/opt/dotfiles/conf/bash/paths.sh
 source /usr/local/opt/dotfiles/conf/bash/prompt.sh
 
