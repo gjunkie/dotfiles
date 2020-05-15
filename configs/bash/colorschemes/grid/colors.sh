3abe6fe2d90c945b47893b8a1d0e1aad58719657
Freddie Carthy <fcarthy@gmail.com>
2020-05-15 16:31:14 -0700

    prepping for bash colorschemes

---
+++ configs/bash/colorschemes/grid/colors.sh
@@ -0,0 +1,19 @@
+
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+DARK_GRAY_FG='[1;30m'
+LIGHT_GRAY_FG='[0;37m'
+LIGHT_BLUE_FG='[1;34m'
+LIGHT_CYAN_FG='[1;36m'
+PURPLE_FG='[1;35m'
+WHITE_FG='[1;37m'
+YELLOW_FG='[0;33m'
+NC='[0m' # No Color
+
