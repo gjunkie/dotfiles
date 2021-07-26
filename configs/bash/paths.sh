c583897fd9ba83b6b2100deff4613f19b19367fe
Freddie Carthy <fcarthy@gmail.com>
2021-07-26 10:56:34 -0700

    update bash files

--- configs/bash/paths.sh
+++ configs/bash/paths.sh
@@ -4,7 +4,6 @@ export PATH=/usr/local/:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/share/npm/bin/:$PATH
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
+export PATH=/Users/${USER}/.gem/ruby/2.6.0/bin:$PATH
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH=$PATH:$HOME/Library/Python/2.7/bin
--- configs/runcom/bashrc.sh
+++ configs/runcom/bashrc.sh
@@ -6,6 +6,3 @@ source ${HOME}/.bash/prompt/prompt.sh
 if [ -f ~/.bash_profile.local ]; then
   source ~/.bash_profile.local
 fi
-
-export EDITOR=vim
-export PATH=/Users/${USER}/.gem/ruby/2.6.0/bin:$PATH
