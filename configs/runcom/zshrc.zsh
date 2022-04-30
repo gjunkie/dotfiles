c9db930e9126fcd76cb9d69d22a041f8d215717b
Federico Carthy <fcarthy@twitter.com>
2022-04-29 17:06:46 -0700

    update zsh prompt

--- configs/runcom/zshrc.zsh
+++ configs/runcom/zshrc.zsh
@@ -8,6 +8,22 @@ ssh_info() {
   [[ "$SSH_CONNECTION" != '' ]] && echo '%(!.%{$fg[red]%}.%{$fg[yellow]%})%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%}:' || echo ''
 }
 
+# contains(string, substring)
+#
+# Returns 0 if the specified string contains the specified substring,
+# otherwise returns 1.
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 0    # $substring is in $string
+    else
+        return 1    # $substring is not in $string
+    fi
+}
+
+
 # Echoes information about Git repository status when inside a Git repository
 git_info() {
 
@@ -18,57 +34,65 @@ git_info() {
   local GIT_LOCATION=${$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD)#(refs/heads/|tags/)}
 
   local AHEAD="%{$fg[red]%}⇡NUM%{$reset_color%}"
-  local BEHIND="%{$fg[cyan]%}⇣NUM%{$reset_color%}"
+  local BEHIND="%{$fg[red]%}⇣NUM%{$reset_color%}"
   local MERGING="%{$fg[magenta]%}⚡︎%{$reset_color%}"
-  local UNTRACKED="%{$fg[red]%}●%{$reset_color%}"
+  local UNTRACKED="%{$fg[cyan]%}●%{$reset_color%}"
   local MODIFIED="%{$fg[green]%}●%{$reset_color%}"
   local STAGED="%{$fg[yellow]%}●%{$reset_color%}"
 
-  local -a DIVERGENCES
+  # local -a DIVERGENCES
   local -a FLAGS
 
-  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_AHEAD" -gt 0 ]; then
-    DIVERGENCES+=( "${AHEAD//NUM/$NUM_AHEAD}" )
-  fi
+  local __git_status=$(git status 2> /dev/null)
 
-  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_BEHIND" -gt 0 ]; then
-    DIVERGENCES+=( "${BEHIND//NUM/$NUM_BEHIND}" )
-  fi
+  # local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+  # if [ "$NUM_AHEAD" -gt 0 ]; then
+  #   DIVERGENCES+=( "${AHEAD//NUM/$NUM_AHEAD}" )
+  # fi
 
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-    FLAGS+=( "$MERGING" )
-  fi
+  # local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+  # if [ "$NUM_BEHIND" -gt 0 ]; then
+  #   DIVERGENCES+=( "${BEHIND//NUM/$NUM_BEHIND}" )
+  # fi
 
-  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    FLAGS+=( "$UNTRACKED" )
-  fi
+  # local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+  # if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+  #   FLAGS+=( "$MERGING" )
+  # fi
 
-  if ! git diff --quiet 2> /dev/null; then
+#   if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+#     FLAGS+=( "$UNTRACKED" )
+#   fi
+# 
+#   if ! git diff --quiet 2> /dev/null; then
+#     FLAGS+=( "$MODIFIED" )
+#   fi
+# 
+#   if ! git diff --cached --quiet 2> /dev/null; then
+#     FLAGS+=( "$STAGED" )
+#   fi
+
+  if contains "${__git_status}" "Changes not staged"; then # if repository is dirty
     FLAGS+=( "$MODIFIED" )
   fi
 
-  if ! git diff --cached --quiet 2> /dev/null; then
+  if contains "${__git_status}" "Untracked files"; then # if there are only untracked files
+    FLAGS+=( "$UNTRACKED" )
+  fi
+
+  if contains "${__git_status}" "Changes to be committed"; then # if there are staged files
     FLAGS+=( "$STAGED" )
   fi
 
   local -a GIT_INFO
-  GIT_INFO+=( "\033[38;5;15m±" )
   [ -n "$GIT_STATUS" ] && GIT_INFO+=( "$GIT_STATUS" )
   [[ ${#DIVERGENCES[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)DIVERGENCES}" )
   [[ ${#FLAGS[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)FLAGS}" )
-  GIT_INFO+=( "\033[38;5;15m$GIT_LOCATION%{$reset_color%}" )
+  GIT_INFO+=( "$GIT_LOCATION%{$reset_color%}" )
   echo "${(j: :)GIT_INFO}"
 
 }
 
-# Use ❯ as the non-root prompt character; # for root
-# Change the prompt character color if the last command had a nonzero exit code
 PS1='
-$(ssh_info)%{$fg[gray]%}[%n] [%~%u] $(git_info)
-%(?.%{$fg[gray]%}.%{$fg[red]%})%(!.#.→)%{$reset_color%} '
-
-# PROMPT=$'%{$fg[gray]%}[%n@%~] → '
-# RPROMPT='%{$(branch_color)%}$vcs_info_msg_0_'
+┌─╼$(ssh_info)%{$fg[cyan]%}[%~%u]%{$reset_color%} $(git_info)
+└─╼%(?.%{$fg[gray]%}.%{$fg[red]%})%(!.#.→)%{$reset_color%} '
