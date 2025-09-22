f02e4229212d01a9348fd4168ff1ce4c28cd8640
Freddie Carthy <fcarthy@netflix.com>
2025-09-22 09:17:31 -0700

    neo tree and telescope config updates

---
+++ configs/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+          -- 'thumbs.db',
+        },
+        never_show = {
+          ".DS_Store",
+          ".cache",
+        },
+      },
+    },
+  },
+}
