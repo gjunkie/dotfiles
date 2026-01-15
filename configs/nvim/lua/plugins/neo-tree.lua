eb7de1dea8407c65c1388c517b159eda08369f38
Freddie Carthy <fcarthy@netflix.com>
2026-01-15 08:54:05 -0800

    fix neo tree icons

--- configs/nvim/lua/plugins/neo-tree.lua
+++ configs/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,35 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    default_component_configs = {
+      diagnostics = {
+        symbols = {
+          hint = "󰌵",
+          info = "",
+          warn = "",
+          error = "",
+        },
+        highlights = {
+          hint = "DiagnosticSignHint",
+          info = "DiagnosticSignInfo",
+          warn = "DiagnosticSignWarn",
+          error = "DiagnosticSignError",
+        },
+      },
+      git_status = {
+        symbols = {
+          added = "✚",
+          modified = "●",
+          deleted = "✖",
+          renamed = "➜",
+          untracked = "◆",
+          ignored = "◌",
+          unstaged = "○",
+          staged = "✔",
+          conflict = "⚠",
+        },
+      },
+    },
     filesystem = {
       filtered_items = {
         visible = true,
@@ -11,12 +40,24 @@ return {
         hide_by_name = {
           ".git",
           ".DS_Store",
-          -- 'thumbs.db',
         },
         never_show = {
           ".DS_Store",
           ".cache",
         },
+      },
+      components = {
+        diagnostics = function(config, node, state)
+          local diag = state.diagnostics_lookup or {}
+          local severity = diag[node:get_id()]
+          if severity then
+            return {
+              text = config.symbols[severity],
+              highlight = config.highlights[severity],
+            }
+          end
+          return {}
+        end,
       },
     },
   },
